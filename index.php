8e7b108e829ca052a1e2e37462406f29078ce40b
Pamungkas Galuh <pamungkaass@gmail.com>
2022-03-10 17:29:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BujangGanteng.com</title>
+    <title>helloworld</title>
 </head>
 <body style="background:darkgray;">
     <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
-        <h1 style="color: blue;">Selamat Datang di JUNAVANDRIYANI chanel</h1>
+        <h1 style="color: blue;">Hello World</h1>
         <h3>Demonstrasi Google App Engine</h3>
     </div>
 </body>
